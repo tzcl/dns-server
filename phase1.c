0037567c2ca3bb4b49466737102b378f20b44aae
Toby Law <toby@tzcl.me>
2021-05-15 22:40:46 +1000

    Added structs for message parsing

--- phase1.c
+++ phase1.c
@@ -2,13 +2,47 @@
 
 #include <arpa/inet.h>
 #include <assert.h>
+#include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 typedef uint8_t byte;
+
+struct header {
+  uint16_t id;
+  uint16_t flags;
+  uint16_t qd_count; // assume to be 1
+  uint16_t an_count;
+  uint16_t ns_count;
+  uint16_t ar_count;
+};
+
+struct question {
+  byte *name;
+  uint16_t type;  // assume to be AAAA records (value of 28)
+  uint16_t class; // assume to IN (value of 1)
+};
+
+struct resource {
+  byte *name;
+  uint16_t type;
+  uint16_t class;
+  uint32_t ttl;
+  uint16_t rd_length;
+  byte *rdata;
+};
+
+struct packet {
+  struct header header;
+  struct question question;
+  struct resource *answers;
+  struct resource *authority;
+  struct resource *additional;
+};
 
 int main(int argc, char *argv[]) {
   uint16_t sz; // stores the length of the packet in network order
