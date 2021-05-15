f97fcda45ca13f3972dd0b1bf4cd6f987d561d5a
Toby Law <toby@tzcl.me>
2021-05-15 18:03:19 +1000

    Can read packet size

--- phase1.c
+++ phase1.c
@@ -1,8 +1,30 @@
 // For testing logging/parsing DNS requests
 
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main() {
-  printf("Hello world!\n");
+typedef uint8_t byte;
+
+int main(int argc, char *argv[]) {
+  uint16_t sz; // stores the length of the packet in network order
+  if (read(STDIN_FILENO, &sz, 2) < 0) {
+    perror("reading size");
+    exit(EXIT_FAILURE);
+  }
+  sz = ntohs(sz); // convert to host order
+
+  byte *buffer = (byte *)malloc(sz);
+  assert(buffer);
+
+  if (read(STDIN_FILENO, buffer, sz) < 0) {
+    perror("reading buffer");
+    exit(EXIT_FAILURE);
+  }
+
   return 0;
 }
